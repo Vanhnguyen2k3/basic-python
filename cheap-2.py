54e41889e6a8cb869917138aeaf9c222d1192a31
Vanhnguyen2k3 <vanhnguyendeverloper@gmail.com>
2025-02-18 22:35:28 +0700

    cheap-2

---
+++ cheap-2.py
@@ -0,0 +1,19 @@
+# Chương trình 2.1
+print("Xin chao", 10, 12.1, "Ket thuc")
+
+# Chương trình 2.2
+x = 20
+print("Xin chao", 10, 12.1, "Ket thuc", x)
+
+# Chương trình 2.3
+print("Xin chao", "Ket thuc", sep="---")
+
+# Chương trình 2.4
+print("Xin chao", "Ket thuc", sep="---", end="!!")
+
+# Chương trình 2.5
+print("Xin chao", "Ket thuc", end="!!", sep="---")
+
+# Chương trình 2.6
+a = 3.14165
+print(" Ket qua la %.2f" %a)
