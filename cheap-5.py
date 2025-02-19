5c43cfced052e17a2f7cea1a2173373971545c23
Vanhnguyen2k3 <vanhnguyendeverloper@gmail.com>
2025-02-19 09:08:16 +0700

    cheap-5

---
+++ cheap-5.py
@@ -0,0 +1,18 @@
+# chương trình 5.1
+a = [1, 4, 5, 7]
+print("So thu nhat", a[0])
+print("So thu hai", a[1])
+
+# Chương trình 5.2
+a = [1, 4, 5, 7]
+N = len(a)
+print("Kich thuoc mang", N)
+for i in range(0, N):
+    print(i)
+
+# Chương trình 5.3
+a = [1, 4, 5, 7]
+N = len(a)
+print("Kich thuoc mang", N)
+for i in range(0,N):
+    print("So thu",i + 1, a[i])
